622d03ad65806b0234d9af85c18feef8946452ef
Katiasfihi <Katiasfihi@users.noreply.github.com>
2021-03-17 11:21:04 +0000

    new commit

--- Experiments.js
+++ Experiments.js
@@ -818,3 +818,16 @@ const three = (one) => {
   return four;
 }}
 console.log(three(one))
+
+//
+function copyArrayAndManipulate(array, instructions) {
+  const output =[];
+  for (let i=0; i < array.length; i++) {
+    output.push(instructions(array[i]));
+  }
+  return output;
+}
+function multiplyBy2(input) { return input * 2}
+const result = copyArrayAndManipulate([1, 2, 3], multiplyBy2)
+
+//
